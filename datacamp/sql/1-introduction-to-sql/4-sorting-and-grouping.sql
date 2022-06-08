c282e0d0ca01d854180dc03c0b86ef86adfee805
dcollis92 <davidphilipcollis@gmail.com>
2022-06-08 17:51:55 -0500

    group by exercises

--- datacamp/sql/1-introduction-to-sql/4-sorting-and-grouping.sql
+++ datacamp/sql/1-introduction-to-sql/4-sorting-and-grouping.sql
@@ -125,7 +125,63 @@ FROM people
 ORDER BY NAME, birthdate;
 
 ----------------------------------------
+-- GROUP BY
+-- Now you know how to sort results! Often you'll need to aggregate results. For example, you might want to count the number of male and female employees in your company. Here, what you want is to group all the males together and count them, and group all the females together and count them. In SQL, GROUP BY allows you to group a result by one or more columns, like so:
+
+SELECT sex, count(*)
+FROM employees
+GROUP BY sex;
+-- This might give, for example:
+
+sex	count
+male	15
+female	19
+
+-- Commonly, GROUP BY is used with aggregate functions like COUNT() or MAX(). Note that GROUP BY always goes after the FROM clause!
 
 ----------------------------------------
+-- GROUP BY practice
+-- As you've just seen, combining aggregate functions with GROUP BY can yield some powerful results!
+
+-- A word of warning: SQL will return an error if you try to SELECT a field that is not in your GROUP BY clause without using it to calculate some kind of value about the entire group.
+
+-- Note that you can combine GROUP BY with ORDER BY to group your results, calculate something about them, and then order your results. For example,
+
+SELECT sex, count(*)
+FROM employees
+GROUP BY sex
+ORDER BY count DESC;
+might return something like
+
+sex	count
+female	19
+male	15
+
+-- because there are more females at our company than males. Note also that ORDER BY always goes after GROUP BY. Let's try some exercises!
+
+-- Get the release year and count of films released in each year.
+
+SELECT release_year, COUNT(*)
+FROM films
+GROUP BY release_year;
+
+-- Get the release year and average duration of all films, grouped by release year.
+
+SELECT release_year, AVG(duration)
+FROM films
+GROUP BY release_year;
+
+-- Get the release year and largest budget for all films, grouped by release year.
+
+SELECT release_year, Max(budget)
+FROM films
+GROUP BY release_year;
+
+-- Get the IMDB score and count of film reviews grouped by IMDB score in the reviews table.
+
+SELECT imdb_score, COUNT(*)
+FROM reviews
+GROUP BY imdb_score;
+
 ----------------------------------------
 ----------------------------------------
