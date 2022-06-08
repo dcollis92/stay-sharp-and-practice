2edf08781ac924989faa1f0a839259fd8f2445e8
dcollis92 <davidphilipcollis@gmail.com>
2022-06-08 15:13:50 -0500

    between filtering exercises

--- datacamp/sql/1-introduction-to-sql/2-filtering-rows.sql
+++ datacamp/sql/1-introduction-to-sql/2-filtering-rows.sql
@@ -190,7 +190,63 @@ AND (language = 'French' OR language = 'Spanish')
 AND gross > 2000000
 
 ----------------------------------------
+-- BETWEEN
+-- As you've learned, you can use the following query to get titles of all films released in and between 1994 and 2000:
+
+-- SELECT title
+-- FROM films
+-- WHERE release_year >= 1994
+-- AND release_year <= 2000;
+-- Checking for ranges like this is very common, so in SQL the BETWEEN keyword provides a useful shorthand for filtering values within a specified range. This query is equivalent to the one above:
+
+-- SELECT title
+-- FROM films
+-- WHERE release_year
+-- BETWEEN 1994 AND 2000;
+-- It's important to remember that BETWEEN is inclusive, meaning the beginning and end values are included in the results!
+
 ----------------------------------------
+-- BETWEEN (2)
+-- Similar to the WHERE clause, the BETWEEN clause can be used with multiple AND and OR operators, so you can build up your queries and make them even more powerful!
+
+-- For example, suppose we have a table called kids. We can get the names of all kids between the ages of 2 and 12 from the United States:
+
+-- SELECT name
+-- FROM kids
+-- WHERE age BETWEEN 2 AND 12
+-- AND nationality = 'USA';
+-- Take a go at using BETWEEN with AND on the films data to get the title and release year of all Spanish language films released between 1990 and 2000 (inclusive) with budgets over $100 million. We have broken the problem into smaller steps so that you can build the query as you go along!
+
+-- Get the title and release year of all films released between 1990 and 2000 (inclusive).
+
+SELECT title, release_year
+FROM films
+WHERE release_year 
+BETWEEN 1990 and 2000
+
+-- Now, build on your previous query to select only films that have budgets over $100 million.
+
+SELECT title, release_year
+FROM films
+WHERE release_year BETWEEN 1990 AND 2000
+AND budget > 100000000;
+
+-- Now restrict the query to only return Spanish language films.
+
+SELECT title, release_year
+FROM films
+WHERE release_year BETWEEN 1990 AND 2000
+AND budget > 100000000
+AND LANGUAGE = 'Spanish';
+
+-- Finally, modify to your previous query to include all Spanish language or French language films with the same criteria as before. Don't forget your parentheses!
+
+SELECT title, release_year
+FROM films
+WHERE release_year BETWEEN 1990 AND 2000
+AND budget > 100000000
+AND (language = 'Spanish' or language = 'French');
+
 ----------------------------------------
 ----------------------------------------
 ----------------------------------------
