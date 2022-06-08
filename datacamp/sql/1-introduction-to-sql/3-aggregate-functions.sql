c105e0692c57774a523454b242f5702d6ddca2db
dcollis92 <davidphilipcollis@gmail.com>
2022-06-08 17:03:31 -0500

    aggregate function practice pt2

--- datacamp/sql/1-introduction-to-sql/3-aggregate-functions.sql
+++ datacamp/sql/1-introduction-to-sql/3-aggregate-functions.sql
@@ -37,6 +37,29 @@ SELECT MAX(duration)
 FROM films;
 
 ----------------------------------------
+-- Aggregate functions practice
+-- Good work. Aggregate functions are important to understand, so let's get some more practice! 
+
+-- Use the SUM() function to get the total amount grossed by all films.
+
+SELECT SUM(gross)
+FROM films;
+
+-- Get the average amount grossed by all films.
+
+SELECT AVG(gross)
+FROM films;
+
+-- Get the amount grossed by the worst performing film.
+
+SELECT MIN(gross)
+FROM films;
+
+-- Get the amount grossed by the best performing film.
+
+SELECT MAX(gross)
+FROM films;
+
 ----------------------------------------
 ----------------------------------------
 ----------------------------------------
