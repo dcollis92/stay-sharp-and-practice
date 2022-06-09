bc725da04c6c1e3cf1e1d330eee7ee36ba63ec12
dcollis92 <davidphilipcollis@gmail.com>
2022-06-08 19:44:19 -0500

    group by exercises pt2

--- datacamp/sql/1-introduction-to-sql/4-sorting-and-grouping.sql
+++ datacamp/sql/1-introduction-to-sql/4-sorting-and-grouping.sql
@@ -184,4 +184,60 @@ FROM reviews
 GROUP BY imdb_score;
 
 ----------------------------------------
+-- GROUP BY practice (2)
+-- Now practice your new skills by combining GROUP BY and ORDER BY with some more aggregate functions!
+
+-- Make sure to always put the ORDER BY clause at the end of your query. You can't sort values that you haven't calculated yet!
+
+-- Get the release year and lowest gross earnings per release year.
+
+SELECT release_year, MIN(gross)
+FROM films
+GROUP BY release_year;
+
+-- Get the language and total gross amount films in each language made.
+
+SELECT LANGUAGE, SUM(gross)
+FROM films
+GROUP BY LANGUAGE;
+
+-- Get the country and total budget spent making movies in each country.
+
+SELECT country, SUM(budget)
+FROM films
+GROUP BY country;
+
+-- Get the release year, country, and highest budget spent making a film for each year, for each country. Sort your results by release year and country.
+
+SELECT release_year, country, MAX(budget)
+FROM films
+GROUP BY release_year, country
+ORDER BY release_year, country;
+
+-- Get the country, release year, and lowest amount grossed per release year per country. Order your results by country and release year.
+
+SELECT country, release_year, MIN(gross)
+FROM films
+GROUP BY release_year, country
+order by country, release_year;
+
+----------------------------------------
+-- HAVING a great time
+-- In SQL, aggregate functions can't be used in WHERE clauses. For example, the following query is invalid:
+
+-- SELECT release_year
+-- FROM films
+-- GROUP BY release_year
+-- WHERE COUNT(title) > 10;
+-- This means that if you want to filter based on the result of an aggregate function, you need another way! That's where the HAVING clause comes in. For example,
+
+SELECT release_year
+FROM films
+GROUP BY release_year
+HAVING COUNT(title) > 10;
+-- shows only those years in which more than 10 films were released.
+
+----------------------------------------
+----------------------------------------
+----------------------------------------
 ----------------------------------------
