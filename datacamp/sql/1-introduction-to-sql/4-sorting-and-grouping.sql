58d5526035ae61bfb82ed21054671c860292557c
dcollis92 <davidphilipcollis@gmail.com>
2022-06-08 19:54:40 -0500

    large query practice pt2

--- datacamp/sql/1-introduction-to-sql/4-sorting-and-grouping.sql
+++ datacamp/sql/1-introduction-to-sql/4-sorting-and-grouping.sql
@@ -287,7 +287,22 @@ GROUP BY release_year
 HAVING AVG(budget) > 60000000
 ORDER BY AVG(gross) DESC;
 
+----------------------------------------
+-- All together now (2)
+-- Great work! Now try another large query. This time, all in one go!
+
+-- Remember, if you only want to return a certain number of results, you can use the LIMIT keyword to limit the number of rows returned
+
+-- Get the country, average budget, and average gross take of countries that have made more than 10 films. Order the result by country name, and limit the number of results displayed to 5. You should alias the averages as avg_budget and avg_gross respectively.
+
+SELECT country, AVG(budget) AS avg_budget, avg(gross) AS avg_gross
+FROM films
+group by country
+HAVING COUNT(title) > 10
+ORDER BY country
+LIMIT 5;
 
 ----------------------------------------
-----------------------------------------
+
+
 ----------------------------------------
