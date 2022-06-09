7c525d4b5871e328a0eb378289eebc9fb5cc3dc6
dcollis92 <davidphilipcollis@gmail.com>
2022-06-08 19:50:51 -0500

    large query practice

--- datacamp/sql/1-introduction-to-sql/4-sorting-and-grouping.sql
+++ datacamp/sql/1-introduction-to-sql/4-sorting-and-grouping.sql
@@ -238,6 +238,56 @@ HAVING COUNT(title) > 10;
 -- shows only those years in which more than 10 films were released.
 
 ----------------------------------------
+-- All together now
+-- Time to practice using ORDER BY, GROUP BY and HAVING together.
+
+-- Now you're going to write a query that returns the average budget and average gross earnings for films in each year after 1990, if the average budget is greater than $60 million.
+
+-- This is going to be a big query, but you can handle it!
+
+-- Get the release year, budget and gross earnings for each film in the films table.
+
+SELECT release_year, budget, gross
+FROM films;
+
+-- Modify your query so that only records with a release_year after 1990 are included.
+
+SELECT release_year, budget, gross
+FROM films
+WHERE release_year > 1990;
+
+-- Remove the budget and gross columns, and group your results by release year.
+
+SELECT release_year
+FROM films
+WHERE release_year > 1990
+GROUP BY release_year;
+
+-- Modify your query to include the average budget and average gross earnings for the results you have so far. Alias the average budget as avg_budget; alias the average gross earnings as avg_gross.
+
+SELECT release_year,AVG(budget) AS avg_budget, AVG(gross) AS avg_gross
+FROM films
+WHERE release_year > 1990
+GROUP BY release_year;
+
+-- Modify your query so that only years with an average budget of greater than $60 million are included.
+
+SELECT release_year, AVG(budget) AS avg_budget, AVG(gross) AS avg_gross
+FROM films
+WHERE release_year > 1990
+GROUP BY release_year
+HAVING AVG(budget) > 60000000;
+
+-- Finally, modify your query to order the results from highest average gross earnings to lowest.
+
+SELECT release_year, AVG(budget) AS avg_budget, AVG(gross) AS avg_gross
+FROM films
+WHERE release_year > 1990
+GROUP BY release_year
+HAVING AVG(budget) > 60000000
+ORDER BY AVG(gross) DESC;
+
+
 ----------------------------------------
 ----------------------------------------
 ----------------------------------------
