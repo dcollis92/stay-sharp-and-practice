acbff8a2e35056d6b98cfce194e028627a790321
dcollis92 <davidphilipcollis@gmail.com>
2022-06-09 09:17:10 -0500

    left join exercises p3

--- datacamp/sql/2-joining-data-in-sql/2-outer-joins-and-cross-joins.sql
+++ datacamp/sql/2-joining-data-in-sql/2-outer-joins-and-cross-joins.sql
@@ -74,6 +74,56 @@ FROM countries AS c
 ORDER BY country DESC;
 
 ----------------------------------------
+-- Left join (3)
+-- You'll now revisit the use of the AVG() function introduced in our introductory SQL course. You will use it in combination with left join to determine the average gross domestic product (GDP) per capita by region in 2010.
+
+-- Begin with a left join with the countries table on the left and the economies table on the right.
+-- Focus only on records with 2010 as the year.
+
+-- Select name, region, and gdp_percapita
+SELECT name, region, gdp_percapita
+-- From countries (alias as c)
+FROM countries AS c
+  -- Left join with economies (alias as e)
+  LEFT JOIN economies AS e
+    -- Match on code fields
+    ON c.code = e.code
+-- Focus on 2010
+WHERE year = 2010;
+
+-- Modify your code to calculate the average GDP per capita AS avg_gdp for each region in 2010.
+-- Select the region and avg_gdp fields.
+
+-- Select fields
+SELECT region, AVG(gdp_percapita) AS avg_gdp
+-- From countries (alias as c)
+FROM countries AS c
+  -- Left join with economies (alias as e)
+  LEFT JOIN economies AS e
+    -- Match on code fields
+    ON c.code = e.code
+-- Focus on 2010
+WHERE year = 2010
+-- Group by region
+GROUP BY region;
+
+-- Arrange this data on average GDP per capita for each region in 2010 from highest to lowest average GDP per capita.
+
+-- Select fields
+SELECT region, AVG(gdp_percapita) AS avg_gdp
+-- From countries (alias as c)
+FROM countries AS c
+  -- Left join with economies (alias as e)
+  LEFT JOIN economies AS e
+    -- Match on code fields
+    ON c.code = e.code
+-- Focus on 2010
+WHERE year = 2010
+-- Group by region
+GROUP BY region
+-- Order by descending avg_gdp
+ORDER BY avg_gdp DESC;
+
 ----------------------------------------
 ----------------------------------------
 ----------------------------------------
