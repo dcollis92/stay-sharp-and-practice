92a3a2f5e7cb2438703d806e165623d8ec2a3265
dcollis92 <davidphilipcollis@gmail.com>
2022-06-08 20:31:34 -0500

    case when and then exercises

--- datacamp/sql/2-joining-data-in-sql/1-introduction-to-joins.sql
+++ datacamp/sql/2-joining-data-in-sql/1-introduction-to-joins.sql
@@ -224,5 +224,34 @@ ON p1.country_code = p2.country_code
 AND p1.year = p2.year - 5;
 
 ----------------------------------------
+-- Case when and then
+-- Often it's useful to look at a numerical field not as raw data, but instead as being in different categories or groups.
+
+-- You can use CASE with WHEN, THEN, ELSE, and END to define a new grouping field.
+
+-- Using the countries table, create a new field AS geosize_group that groups the countries into three groups:
+
+-- If surface_area is greater than 2 million, geosize_group is 'large'.
+-- If surface_area is greater than 350 thousand but not larger than 2 million, geosize_group is 'medium'.
+-- Otherwise, geosize_group is 'small'.
+
+SELECT name, continent, code, surface_area,
+-- First case
+CASE WHEN surface_area > 2000000 THEN 'large'
+-- Second case
+WHEN surface_area > 350000 THEN 'medium'
+-- Else clause + end
+ELSE 'small' END
+-- Alias name
+AS geosize_group
+-- From table
+FROM countries;
+
+----------------------------------------
+
+
+----------------------------------------
+----------------------------------------
+----------------------------------------
 ----------------------------------------
 ----------------------------------------
