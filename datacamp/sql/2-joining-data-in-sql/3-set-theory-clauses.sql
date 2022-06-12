0731be592a452a1fb26afd72ec6fcfe04d214159
dcollis92 <davidphilipcollis@gmail.com>
2022-06-12 07:26:07 -0500

    union exercise

--- datacamp/sql/2-joining-data-in-sql/3-set-theory-clauses.sql
+++ datacamp/sql/2-joining-data-in-sql/3-set-theory-clauses.sql
@@ -27,7 +27,43 @@
 -- that UNION and UNION ALL clauses do not do the lookup step that JOINs do. They simply stack records on top of each other from one table to the next.
 
 ----------------------------------------
+-- Union
+-- You have two new tables, economies2010 and economies2015, available to you. The economies table is also included for reference.
 
+-- Combine the two new tables into one table containing all of the fields in economies2010.
+-- Sort this resulting single table by country code and then by year, both in ascending order.
+
+-- Select fields from 2010 table
+SELECT *
+  -- From 2010 table
+  FROM economies2010
+	-- Set theory clause
+	UNION
+-- Select fields from 2015 table
+SELECT *
+  -- From 2015 table
+  FROM economies2015
+-- Order by code and year
+ORDER BY code, year;
 
 ----------------------------------------
+-- Union (2)
+-- UNION can also be used to determine all occurrences of a field across multiple tables. Try out this exercise with no starter code.
+
+-- Determine all (non-duplicated) country codes in either the cities or the currencies table. The result should be a table with only one field called country_code.
+-- Sort by country_code in alphabetical order.
+
+-- Select field
+SELECT country_code
+  -- From cities
+  FROM cities
+	-- Set theory clause
+	UNION
+-- Select field
+SELECT code
+  -- From currencies
+  FROM currencies
+-- Order by country_code
+ORDER BY country_code;
+
 ----------------------------------------
