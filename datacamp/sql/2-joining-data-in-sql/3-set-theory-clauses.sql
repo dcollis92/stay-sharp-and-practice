d2bb342c763efaa35c6166abd27d3fcd748b4f45
dcollis92 <davidphilipcollis@gmail.com>
2022-06-12 10:42:55 -0500

    intersect exercise pt2

--- datacamp/sql/2-joining-data-in-sql/3-set-theory-clauses.sql
+++ datacamp/sql/2-joining-data-in-sql/3-set-theory-clauses.sql
@@ -127,3 +127,22 @@ SELECT country_code, year
 ORDER BY code, year;
 
 ----------------------------------------
+-- Intersect (2)
+-- As you think about major world cities and their corresponding country, you may ask which countries also have a city with the same name as their country name?
+
+-- Use INTERSECT to answer this question with countries and cities!
+
+-- Select fields
+SELECT name 
+  -- From countries
+  FROM countries
+	-- Set theory clause
+	INTERSECT
+-- Select fields
+SELECT name 
+  -- From cities
+  FROM cities;
+
+----------------------------------------
+----------------------------------------
+----------------------------------------
