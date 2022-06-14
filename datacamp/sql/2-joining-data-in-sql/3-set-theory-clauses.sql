52d281168178e2ef0315501381a7d36fce99f3e0
dcollis92 <davidphilipcollis@gmail.com>
2022-06-13 20:43:31 -0500

    anti-join exercise

--- datacamp/sql/2-joining-data-in-sql/3-set-theory-clauses.sql
+++ datacamp/sql/2-joining-data-in-sql/3-set-theory-clauses.sql
@@ -288,4 +288,61 @@ ORDER BY name;
 -- A: DISTINCT
 
 ----------------------------------------
+-- Diagnosing problems using anti-join
+-- Another powerful join in SQL is the anti-join. It is particularly useful in identifying which records are causing an incorrect number of records to appear in join queries.
+
+-- You will also see another example of a subquery here, as you saw in the first exercise on semi-joins. Your goal is to identify the currencies used in Oceanian countries!
+
+-- Begin by determining the number of countries in countries that are listed in Oceania using SELECT, FROM, and WHERE.
+
+-- Select statement
+SELECT COUNT(countries)
+  -- From countries
+  FROM countries
+-- Where continent is Oceania
+WHERE continent = 'Oceania';
+
+-- Complete an inner join with countries AS c1 on the left and currencies AS c2 on the right to get the different currencies used in the countries of Oceania.
+-- Match ON the code field in the two tables.
+-- Include the country code, country name, and basic_unit AS currency.
+-- Observe the query result and make note of how many different countries are listed here.
+
+-- Select fields (with aliases)
+SELECT c1.code, c1.name, basic_unit AS currency
+  -- From countries (alias as c1)
+  FROM countries AS c1
+  	-- Join with currencies (alias as c2)
+  	INNER JOIN currencies AS c2
+    -- Match on code
+    ON c1.code = c2.code
+-- Where continent is Oceania
+WHERE continent = 'Oceania';
+
+-- Note that not all countries in Oceania were listed in the resulting inner join with currencies. Use an anti-join to determine which countries were not included!
+
+-- Use NOT IN and (SELECT code FROM currencies) as a subquery to get the country code and country name for the Oceanian countries that are not included in the currencies table.
+
+-- Select fields
+SELECT c1.code, c1.name
+  -- From Countries
+  FROM countries AS c1
+  -- Where continent is Oceania
+  WHERE continent = 'Oceania'
+  	-- And code not in
+  	AND code NOT IN
+  	-- Subquery
+  	(SELECT code 
+	FROM currencies);
+
+----------------------------------------
+-- Set theory challenge
+-- Congratulations! You've now made your way to the challenge problem for this third chapter. Your task here will be to incorporate two of UNION/UNION ALL/INTERSECT/EXCEPT to solve a challenge involving three tables.
+
+-- In addition, you will use a subquery as you have in the last two exercises! This will be great practice as you hop into subqueries more in Chapter 4!
+
+-- Identify the country codes that are included in either economies or currencies but not in populations.
+-- Use that result to determine the names of cities in the countries that match the specification in the previous instruction.
+
+----------------------------------------
+----------------------------------------
 ----------------------------------------
