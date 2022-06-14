00afe69390547c5ff4a42a75369e8eca2b6936b5
dcollis92 <davidphilipcollis@gmail.com>
2022-06-13 20:54:42 -0500

    set theory challenge

--- datacamp/sql/2-joining-data-in-sql/3-set-theory-clauses.sql
+++ datacamp/sql/2-joining-data-in-sql/3-set-theory-clauses.sql
@@ -343,6 +343,26 @@ SELECT c1.code, c1.name
 -- Identify the country codes that are included in either economies or currencies but not in populations.
 -- Use that result to determine the names of cities in the countries that match the specification in the previous instruction.
 
+-- Select the city name
+SELECT c1.name
+  -- Alias the table where city name resides
+  FROM cities AS c1
+  -- Choose only records matching the result of multiple set theory clauses
+  WHERE country_code IN
+(
+    -- Select appropriate field from economies AS e
+    SELECT e.code
+    FROM economies AS e
+    -- Get all additional (unique) values of the field from currencies AS c2  
+    UNION
+    SELECT DISTINCT c2.code
+    FROM currencies AS c2  
+    -- Exclude those appearing in populations AS p
+    EXCEPT
+    SELECT p.country_code
+    FROM populations AS p
+);
+
 ----------------------------------------
 ----------------------------------------
 ----------------------------------------
