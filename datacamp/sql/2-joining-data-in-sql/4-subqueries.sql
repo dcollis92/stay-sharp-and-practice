7bf3e9ac80769d3591681e18c0f1a20f4af55565
dcollis92 <davidphilipcollis@gmail.com>
2022-06-14 08:11:33 -0500

    sub query inside from exercise

--- datacamp/sql/2-joining-data-in-sql/4-subqueries.sql
+++ datacamp/sql/2-joining-data-in-sql/4-subqueries.sql
@@ -129,7 +129,39 @@ LIMIT 9;
 -- You are very close to the end of the course. Awesome work on making it this far! The last remaining exercises are designed to really test your understanding of the material. You may be a bit frustrated but stick with it and you'll take the knowledge gained in these problems with you as you continue to develop your SQL skills. See you in a bit for the course review video!
 
 ----------------------------------------
+-- Subquery inside from
+-- The last type of subquery you will work with is one inside of FROM.
 
+-- You will use this to determine the number of languages spoken for each country, identified by the country's local name! (Note this may be different than the name field and is stored in the local_name field.)
+
+-- Begin by determining for each country code how many languages are listed in the languages table using SELECT, FROM, and GROUP BY.
+-- Alias the aggregated field as lang_num.
+
+-- Select fields (with aliases)
+SELECT code, COUNT(*) AS lang_num
+  -- From languages
+  FROM languages
+-- Group by code
+GROUP BY code;
+
+-- Include the previous query (aliased as subquery) as a subquery in the FROM clause of a new query.
+-- Select the local name of the country from countries.
+-- Also, select lang_num from subquery.
+-- Make sure to use WHERE appropriately to match code in countries and in subquery.
+-- Sort by lang_num in descending order.
+
+-- Select fields
+SELECT local_name, subquery.lang_num
+  -- From countries
+  FROM countries, 
+  	-- Subquery (alias as subquery)
+  	(SELECT code, COUNT(*) AS lang_num
+  	 FROM languages
+  	 GROUP BY code) AS subquery
+  -- Where codes match
+  WHERE countries.code = subquery.code
+-- Order by descending number of languages
+ORDER BY subquery.lang_num DESC;
 
 ----------------------------------------
 ----------------------------------------
