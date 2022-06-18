ac2f5c4662ac44d1f72cb09becf5e09c7b1cc7c5
dcollis92 <davidphilipcollis@gmail.com>
2022-06-17 21:19:33 -0500

    intro sql practice q4

--- datacamp/sql/1-introduction-to-sql/practice.sql
+++ datacamp/sql/1-introduction-to-sql/practice.sql
@@ -23,3 +23,31 @@ SELECT COUNT(YEAR) FROM states;
 -- A: The number of non-missing values in the YEAR column. 
 
 ----------------------------------------
+-- Which keyword is used to group a result by one or more columns?
+
+GROUP BY
+
+----------------------------------------
+-- How would you get the number of movies released after the year 2000?
+
+SELECT COUNT(*)
+FROM films
+WHERE release_year > 2000;
+
+----------------------------------------
+-- Select the code to return the output
+
+release_year
+1916
+1920
+1925
+1927
+1929
+
+----
+
+SELECT release_year	
+FROM films	
+LIMIT 5;
+
+----------------------------------------
