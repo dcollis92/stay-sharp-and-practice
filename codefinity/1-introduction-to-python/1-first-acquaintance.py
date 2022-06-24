572e0f25e94bd4d3f0cef67a056081b076b822b7
dcollis92 <davidphilipcollis@gmail.com>
2022-06-24 16:31:44 -0500

    add codefinity python ch1

---
+++ codefinity/1-introduction-to-python/1-first-acquaintance.py
@@ -0,0 +1,73 @@
+# The Very First Steps
+# Hello learner!
+
+# For the first steps, let's try to print something! To make Python print something, use the print() function. To print your desired text, you should place it inside brackets " " or ' ', otherwise Python will try to find a variable with that name (as to be discussed later). Thus, if you want to print a sentence, you should write and compile the following piece of code:
+
+# print('Some message')
+# This will print
+
+# Some message
+# into console. Try it by yourself!
+
+# Task
+
+# Print "Hello world!" into the console.
+
+print("Hello world!")
+
+# ----------------------------------------
+# Comments
+# Well done! Firstly, it's important to note that the bigger the code is, the harder it is to understand the separate piece of code, even if it was written by yourself. Therefore, to solve it, learn to leave comments in your code such as instructions of what this piece of code does or something understandable to another person. To mark something as a comment, begin the respective string with #.
+
+# For example,
+
+# Input:
+# print('Message 1')
+# print('Message 2')
+# Output:
+# Message 1
+# Message 2
+# But if we place the hash # before print, this line will not be compiled.
+
+# Input:
+# #print('Message 1')
+# print('Message 2')
+# Output:
+# Message 2
+
+# Task
+
+# Look at the code. Comment on the first two lines so that they won't be printed. Please, do not change the text in the code.
+
+# Click Submit Task button below the code to check the solution of this task.
+
+# print("This is the comment")
+# print("Make this line comment too by adding hash")
+print("This is the only string printed!")
+
+# ----------------------------------------
+# Some Simple Math
+# Nice! Python also can calculate math expressions. The syntax is very simple: use + for addition, - for subtraction, * for multiplication, and / for division.
+
+# This time, don't forget to use print if you want your result to be displayed. Also, there is no need to use "" brackets, as you want to calculate math expression.
+
+# For example, let's calculate some basic stuff:
+
+# example-icon
+# Example
+# # Addition
+print(1039 + 948)
+
+# # Subtraction
+print(10484 - 9274)
+
+# # Division
+print(95873/472)
+
+# Task
+
+# Calculate the following expressions and print the results:
+
+# 2580 plus 198
+# 23 multiplied by 42
+# 1512 divided by 27
