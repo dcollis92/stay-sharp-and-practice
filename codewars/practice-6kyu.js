e62ff28d0bbb32b092a3c53cbb06b2970bb7e7a8
dcollis92 <davidphilipcollis@gmail.com>
2022-06-30 09:37:42 -0500

    solve multiples of 3 or 5

--- codewars/practice-6kyu.js
+++ codewars/practice-6kyu.js
@@ -67,3 +67,32 @@ function groupIn10s(...numbers) {
 // index 0 = 1st array (0-9)
 // index 1 = 2nd array (10-19) 
 // and so on and so on
+
+/*---------------------------------------------*/
+/* 
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in. Additionally, if the number is negative, return 0 (for languages that do have them).
+
+Note: If the number is a multiple of both 3 and 5, only count it once.
+*/
+
+  function solution(number){
+    let result = 0
+    
+    if (number <= 0) {
+      return 0
+    }
+  
+    for (let i = 1; i < number; i++) {
+      if (i % 3 === 0 || i % 5 === 0) {
+        result += i
+      }
+    }
+    return result
+  }
+
+
+// a single number is passed through
+// finding all multiples of 3 and 5 below the number passed in
+// summing those numbers
