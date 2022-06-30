ca2c84c88c4a05faaf0d9026a20a1f46097d04c3
dcollis92 <davidphilipcollis@gmail.com>
2022-06-30 10:02:59 -0500

    charMap and findOdds

--- codewars/practice-6kyu.js
+++ codewars/practice-6kyu.js
@@ -96,3 +96,31 @@ Note: If the number is a multiple of both 3 and 5, only count it once.
 // a single number is passed through
 // finding all multiples of 3 and 5 below the number passed in
 // summing those numbers
+
+/*---------------------------------------------*/
+/* 
+Given an array of integers, find the one that appears an odd number of times.
+
+There will always be only one integer that appears an odd number of times.
+
+Examples
+[7] should return 7, because it occurs 1 time (which is odd).
+[0] should return 0, because it occurs 1 time (which is odd).
+[1,1,2] should return 2, because it occurs 1 time (which is odd).
+[0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+[1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+*/
+
+function findOdd(A) {
+  const numMap = {}
+
+  for (let num of A) {
+    numMap[num] = numMap[num] + 1 || 1
+  }
+
+  for (let num in numMap) {
+    if (numMap[num] % 2 !== 0) {
+      return parseInt(num)
+    }
+  }  
+}
