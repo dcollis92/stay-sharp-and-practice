357a613b3ada002675501a025b0ca4f8faa8aae6
dcollis92 <davidphilipcollis@gmail.com>
2022-07-13 09:11:13 -0500

    solve maxSubArray

--- leetcode/blind-75/week-1-sequences/6-maxSubArray.js
+++ leetcode/blind-75/week-1-sequences/6-maxSubArray.js
@@ -21,7 +21,7 @@ Example 3:
 
 Constraints:
   1 <= nums.length <= 10**5
-  -104 <= nums[i] <= 10**4
+  -10**4 <= nums[i] <= 10**4
 
 Follow up: If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
 */
@@ -30,6 +30,34 @@ Follow up: If you have figured out the O(n) solution, try coding another solutio
  * @param {number[]} nums
  * @return {number}
  */
- var maxSubArray = function(nums) {
-    
-};
+
+// brute force solution = 0(n**2)
+// var maxSubArray = function(nums) {
+//   let maxSum = Number.MIN_SAFE_INTEGER
+
+//   for (let i = 0; i < nums.length; i++) {
+//     let currentSum = 0;
+//     for (let j = i; j < nums.length; j++) {
+//       currentSum += nums[j]
+//       maxSum = Math.max(maxSum, currentSum)
+//     }
+//   }
+//   return maxSum
+// };
+
+// kadane's algorithm = O(n)
+var maxSubArray = function(nums) {
+  // due to working with negative integers, we use MIN_SAFE_INTEGER to compare against
+  let maxSum = Number.MIN_SAFE_INTEGER;
+  let currentSum = 0;
+
+  for(let i = 0; i <nums.length; i++) {
+    // each loop compares if adding the next integer increases the value or not
+    currentSum = Math.max(currentSum + nums[i], nums[i])
+    // each loop compares the current value and reassigns the maxSum if it has increased
+    maxSum = Math.max(maxSum, currentSum)
+  }
+  return maxSum
+}
+
+console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
