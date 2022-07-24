693ccd0df6d99a714076dd6914bb435ae4b95098
dcollis92 <davidphilipcollis@gmail.com>
2022-07-23 20:34:40 -0500

    update file with prettier

--- charMap.js
+++ charMap.js
@@ -7,25 +7,24 @@ string > object (keys= char, value= number of times)
 
 */
 
-const string = "Hello There!"
-const charMap = {}
+const string = "Hello There!";
+const charMap = {};
 
 for (let char of string) {
-  charMap[char] = charMap[char] + 1 || 1  
+  charMap[char] = charMap[char] + 1 || 1;
 }
 
-console.log(charMap)
+console.log(charMap);
 
 // ITERATING THROUGH A CHARACTER MAP
 
 // counting
-let max = 0
-let maxChar = ''
-    
+let max = 0;
+let maxChar = "";
+
 for (let char in charMap) {
   if (charMap[char] > max) {
-    max = charMap[char]
-    maxChar = char
+    max = charMap[char];
+    maxChar = char;
   }
-  
 }
