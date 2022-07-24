234e50a420d35b37cdc818427b96b96df612bc6f
dcollis92 <davidphilipcollis@gmail.com>
2022-07-23 20:28:14 -0500

    update file with prettier

--- codewars/practice-8kyu.js
+++ codewars/practice-8kyu.js
@@ -2,35 +2,34 @@
 
 // For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
 
-function squareSum(numbers){
+function squareSum(numbers) {
   // initalize variables
-    let sum = 0;
-    for (let i = 0; i < numbers.length; i++) {
-      sum += (numbers[i] ** 2) 
-    }
-    return sum;
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i] ** 2;
   }
-  
-  
-  // for each number in the numbers array
-  // square each number, and then add the sum of those squared numbers together
-  // return the sum
+  return sum;
+}
 
-  // alt solutions
+// for each number in the numbers array
+// square each number, and then add the sum of those squared numbers together
+// return the sum
 
-  function squareSum(numbers){
-    return numbers.reduce(function(sum, n){
-      return (n*n) + sum;
-    }, 0)
+// alt solutions
+
+function squareSum(numbers) {
+  return numbers.reduce(function (sum, n) {
+    return n * n + sum;
+  }, 0);
+}
+
+function squareSum(numbers) {
+  let result = 0;
+  for (let n of numbers) {
+    result += n * n;
   }
-
-  function squareSum(numbers) {
-    let result = 0
-    for (let n of numbers) {
-      result += n * n
-    }
-    return result
-  }
+  return result;
+}
 
 /*---------------------------------------------*/
 /* 
@@ -42,5 +41,5 @@ Note: only positive integers will be tested.
 */
 
 function otherAngle(a, b) {
-  return 180-(a+b);
+  return 180 - (a + b);
 }
