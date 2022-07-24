33b908d3121a8476750d6867e0158f572ea92711
dcollis92 <davidphilipcollis@gmail.com>
2022-07-23 20:17:08 -0500

    adjust numOfIslands with prettier

--- leetcode/blind-75/week-2-data-structures/7-numberOfIslands.js
+++ leetcode/blind-75/week-2-data-structures/7-numberOfIslands.js
@@ -36,45 +36,47 @@ Constraints:
  * @param {character[][]} grid
  * @return {number}
  */
-var numIslands = function(grid) {
+var numIslands = function (grid) {
   // count num of islands
-  let counter = 0
+  let counter = 0;
 
   // helper function for ForLoops below
-  const depthFirstSearch = function(i, j) {
+  const depthFirstSearch = function (i, j) {
     // edge case
     if (
       i >= 0 &&
       j >= 0 &&
       i < grid.length &&
       j < grid[i].length &&
-      grid[i][j] === '1'
+      grid[i][j] === "1"
     ) {
       // mark element as visited
-      grid[i][j] = '0'
+      grid[i][j] = "0";
       // visited all surroundings
-      depthFirstSearch(i + 1, j) //bottom
-      depthFirstSearch(i, j + 1) //right
-      depthFirstSearch(i - 1, j) //top
-      depthFirstSearch(i, j - 1) //left
+      depthFirstSearch(i + 1, j); //bottom
+      depthFirstSearch(i, j + 1); //right
+      depthFirstSearch(i - 1, j); //top
+      depthFirstSearch(i, j - 1); //left
     }
-  }
+  };
 
   for (let i = 0; i < grid.length; i++) {
     for (let j = 0; j < grid[i].length; j++) {
       // once it finds the island
-      if (grid[i][j] === '1') {
-        counter += 1
-        depthFirstSearch(i, j)
+      if (grid[i][j] === "1") {
+        counter += 1;
+        depthFirstSearch(i, j);
       }
-    } 
+    }
   }
-  return counter
+  return counter;
 };
 
-console.log(numIslands([
-  ["1","1","0","0","0"],
-  ["1","1","0","0","0"],
-  ["0","0","1","0","0"],
-  ["0","0","0","1","1"]
-]))
+console.log(
+  numIslands([
+    ["1", "1", "0", "0", "0"],
+    ["1", "1", "0", "0", "0"],
+    ["0", "0", "1", "0", "0"],
+    ["0", "0", "0", "1", "1"],
+  ])
+);
