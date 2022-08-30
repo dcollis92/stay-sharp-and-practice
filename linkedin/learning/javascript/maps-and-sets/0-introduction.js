699eb6d67ab5218fc5c780023a12f4acb8317f1e
dcollis92 <davidphilipcollis@gmail.com>
2022-08-29 21:34:46 -0500

    maps and sets LI learning course

---
+++ linkedin/learning/javascript/maps-and-sets/0-introduction.js
@@ -0,0 +1,11 @@
+/* 
+Introduction
+
+- Get to know Javascript maps and sets
+Transcript:
+One of the beautiful things about programming is how it is constantly evolving. JavaScript is no exception. Over time, JavaScript has grown and many aspects have been added including maps and sets. This course was created to help you step up your JavaScript skills and learn the ins and outs of maps and sets. Hi, I'm Jamie Pittman and I'm a front end software engineer. Join me and let's learn more about JavaScript maps and sets.
+
+- What you should know
+Transcript:
+This course provides a beginner to intermediate level overview of JavaScript maps and sets. You absolutely do not need to be an expert or senior level developer, but this course does require a basic understanding of JavaScript. Advanced knowledge is not required for this course. If you're just starting out, this will be a great way to further your knowledge of JavaScript, specifically with maps and sets. If you have a few years of development experience under your belt, this will help you sharpen your skills. You should be comfortable using and understanding arrays, objects and functions. Additionally, you should be familiar with how to use your favorite text editor and your terminal. For this course, I'll be using VS Code and it's built-in terminal. Please use whatever text editor and terminal that you feel most comfortable using. It does not have to be VS Code. In order to run the code, we will be writing. You will need to install Node on your machine if it is not already. For more information on how to get set up with Node JS, please go to nodejs.org/en/download. We will be using Node to run our exercise files and see maps and sets at work. In order to run the files in Node, you'll need to navigate to the directory that the files are stored in and then type node, the name of the file. Make sure the file extension JavaScript, or JS, for all of these is included when you type in the file name. That's all you'll need for this course. Now let's get started.
+*/
