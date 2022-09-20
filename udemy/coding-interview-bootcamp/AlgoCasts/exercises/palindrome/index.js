7b926eefe56617f51476d1e0b81ce9293e32b576
dcollis92 <davidphilipcollis@gmail.com>
2022-09-20 16:04:45 -0500

    palindrome solution 1

--- udemy/coding-interview-bootcamp/AlgoCasts/exercises/palindrome/index.js
+++ udemy/coding-interview-bootcamp/AlgoCasts/exercises/palindrome/index.js
@@ -7,6 +7,14 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  /*
+   * Solution 1
+   * use prior string reverse method
+   * compared strict equality
+   * return true if equal
+   */
+  return str === str.split("").reverse().join("");
+}
 
 module.exports = palindrome;
