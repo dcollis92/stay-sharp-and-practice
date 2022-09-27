f5a804eff0a823101b17e94328ec8dfae8656360
dcollis92 <davidphilipcollis@gmail.com>
2022-09-27 13:30:20 -0500

    anagrams solution 2

--- udemy/coding-interview-bootcamp/AlgoCasts/exercises/9-anagrams/index.js
+++ udemy/coding-interview-bootcamp/AlgoCasts/exercises/9-anagrams/index.js
@@ -9,47 +9,45 @@
 //   anagrams('Hi there', 'Bye there') --> False
 
 function anagrams(stringA, stringB) {
-  /* My Solution
-   * RegEx
-   * /[^\w]/g to clean up the string to only characters
-   * .toLowerCase()
-   */
-  // TODO compare my solution
-  //! if (
-  //!   stringA.replace(/[^\w]/g, "").toLowerCase().split('').sort().join('') ===
-  //!   stringB.replace(/[^\w]/g, "").toLowerCase().split('').sort().join('')
-  //! ) {
-  //!   return true;
-  //! }
-
   /*
    * Solution 1
    */
+  //! const aCharMap = buildCharMap(stringA);
+  //! const bCharMap = buildCharMap(stringB);
+  //! if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+  //!   return false;
+  //! }
+  //! for (let char in aCharMap) {
+  //!   if (aCharMap[char] !== bCharMap[char]) {
+  //!     return false;
+  //!   }
+  //! }
+  //! return true;
 
-  const aCharMap = buildCharMap(stringA);
-  const bCharMap = buildCharMap(stringB);
+  /* Solution 2
+   * RegEx
+   * /[^\w]/g to clean up the string to only characters
+   * .toLowerCase()
+   * .split("") to split the string into an array
+   */
 
-  if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
-    return false;
+  if (cleanString(stringA) === cleanString(stringB)) {
+    return true;
   }
-
-  for (let char in aCharMap) {
-    if (aCharMap[char] !== bCharMap[char]) {
-      return false;
-    }
-  }
-  return true;
 }
 
-// helper function for Solution 1
-function buildCharMap(str) {
-  const charMap = {};
+// Solution 1 helper function
+//! function buildCharMap(str) {
+//!   const charMap = {};
+//!   for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
+//!     charMap[char] = charMap[char] + 1 || 1;
+//!   }
+//!   return charMap;
+//! }
 
-  for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
-    charMap[char] = charMap[char] + 1 || 1;
-  }
-
-  return charMap;
+// Solution 2 helper function
+function cleanString(str) {
+  return str.replace(/[^\w]/g, "").toLowerCase().split("").sort().join("");
 }
 
 module.exports = anagrams;
