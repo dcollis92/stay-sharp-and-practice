f72b91e5b0c13b7ba1f21c8d0a07e64b3c4bdf27
dcollis92 <davidphilipcollis@gmail.com>
2022-10-10 16:56:39 -0500

    pyramid solution 1

--- udemy/coding-interview-bootcamp/AlgoCasts/exercises/12-pyramid/index.js
+++ udemy/coding-interview-bootcamp/AlgoCasts/exercises/12-pyramid/index.js
@@ -15,7 +15,35 @@
 //       '#####'
 
 function pyramid(n) {
-  
+  /* Solution 1
+   * From 0 to n (iterate through rows)
+   * Create an empty string, 'level'
+   * From 0 to ??? (columns)
+   * IF the column should have a #
+   * Add a '#' to 'level'
+   * ELSE
+   * Add a space to 'level'
+   * Console.log 'stair'
+   */
+
+  for (let row = 0; row < n; row++) {
+    let level = "";
+    // number of columns is n*2 - 1
+    // ex: n = 3, 3*2 - 1 = 5
+    // ex: n = 4, 4*2 - 1 = 7
+
+    for (let col = 0; col < 2 * n - 1; col++) {
+      const midpoint = Math.floor((2 * n - 1) / 2);
+      if (midpoint - row <= col && midpoint + row >= col) {
+        // if the midpoint - row <= col, then we are in the left side of the pyramid
+        // if the midpoint + row >= col, then we are in the right side of the pyramid
+        level += "#";
+      } else {
+        level += " ";
+      }
+    }
+    console.log(level);
+  }
 }
 
 module.exports = pyramid;
