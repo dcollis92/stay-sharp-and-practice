6012512c90d861d08272561fcdad72027e69ab0c
dcollis92 <davidphilipcollis@gmail.com>
2023-01-07 12:46:20 -0600

    vowels solution 2

--- udemy/coding-interview-bootcamp/AlgoCasts/exercises/13-vowels/index.js
+++ udemy/coding-interview-bootcamp/AlgoCasts/exercises/13-vowels/index.js
@@ -7,22 +7,30 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-// Solution 1
+//! // Solution 1
+//! function vowels(str) {
+//!   // create a counter variable
+//!   let count = 0;
+
+//!   // create an array of vowels
+//!   const checker = ["a", "e", "i", "o", "u"];
+
+//!   // loop through the string
+//!   for (let char of str.toLowerCase()) {
+//!     // if the character is in the array, increment the counter
+//!     if (checker.includes(char)) {
+//!       count++;
+//!     }
+//!   }
+//!   return count;
+//! }
+
+// Solution 2 w/ regex
 function vowels(str) {
-  // create a counter variable
-  let count = 0;
-
-  // create an array of vowels
-  const checker = ["a", "e", "i", "o", "u"];
-
-  // loop through the string
-  for (let char of str.toLowerCase()) {
-    // if the character is in the array, increment the counter
-    if (checker.includes(char)) {
-      count++;
-    }
-  }
-  return count;
+  // create a match variable
+  const matches = str.match(/[aeiou]/gi);
+  // if there are matches, return the length of the matches, else return 0
+  return matches ? matches.length : 0;
 }
 
 module.exports = vowels;
