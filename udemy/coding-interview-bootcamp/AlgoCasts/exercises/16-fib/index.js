d05057d00c86d8a0cfa526b621cc1b176579cbff
dcollis92 <davidphilipcollis@gmail.com>
2023-01-08 13:37:38 -0600

    fibonacci solution 2

--- udemy/coding-interview-bootcamp/AlgoCasts/exercises/16-fib/index.js
+++ udemy/coding-interview-bootcamp/AlgoCasts/exercises/16-fib/index.js
@@ -8,19 +8,39 @@
 // Example:
 //   fib(4) === 3
 
+//! function fib(n) {
+//!   // Solution 1 - Iterative
+//!   // create an array to hold the fibonacci numbers w/ the first two numbers to generate the rest
+//!   const result = [0, 1];
+//!   // loop through the array
+//!   for (let i = 2; i <= n; i++) {
+//!     // push the sum of the previous two numbers into the array
+//!     const a = result[i - 1];
+//!     const b = result[i - 2];
+//!     result.push(a + b);
+//!   }
+//!   // return the last number in the array
+//!   return result[n];
+//! }
+
 function fib(n) {
-  // Solution 1
-  // create an array to hold the fibonacci numbers w/ the first two numbers to generate the rest
-  const result = [0, 1];
-  // loop through the array
-  for (let i = 2; i <= n; i++) {
-    // push the sum of the previous two numbers into the array
-    const a = result[i - 1];
-    const b = result[i - 2];
-    result.push(a + b);
+  // Solution 2 - Recursive
+  // if n is less than 2, return n
+  if (n < 2) {
+    return n;
   }
-  // return the last number in the array
-  return result[n];
+
+  // return the sum of the previous two numbers
+  return fib(n - 1) + fib(n - 2);
+
+  // Diagram of the recursive solution:
+  // [fib(0)] -> 0
+  // [fib(1)] -> 1
+  // [fib(2)] -> [fib(1)] + [fib(0)] -> 1 + 0 -> 1
+  // [fib(3)] -> [fib(2)] + [fib(1)] -> 1 + 1 -> 2
+  // [fib(4)] -> [fib(3)] + [fib(2)] -> 2 + 1 -> 3
+  // [fib(5)] -> [fib(4)] + [fib(3)] -> 3 + 2 -> 5
+  // [fib(6)] -> [fib(5)] + [fib(4)] -> 5 + 3 -> 8
 }
 
 module.exports = fib;
