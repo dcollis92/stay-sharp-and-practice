c7afdf1dac15420c84dca4da23346359f5d7b2b7
dcollis92 <davidphilipcollis@gmail.com>
2023-01-16 15:59:28 -0600

    qfroms solution

--- udemy/coding-interview-bootcamp/AlgoCasts/exercises/20-qfroms/index.js
+++ udemy/coding-interview-bootcamp/AlgoCasts/exercises/20-qfroms/index.js
@@ -14,6 +14,58 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+  constructor() {
+    // create a data array assigning it to the queue
+    this.first = new Stack();
+    this.second = new Stack();
+  }
+
+  // create an add method
+  add(record) {
+    this.first.push(record);
+  }
+
+  // create a remove method
+  remove() {
+    // while the first stack has data
+    while (this.first.peek()) {
+      // push the first stack data into the second stack
+      this.second.push(this.first.pop());
+    }
+
+    // pop the second stack data
+    const record = this.second.pop();
+    // while the second stack has data
+    while (this.second.peek()) {
+      // push the second stack data into the first stack
+      this.first.push(this.second.pop());
+    }
+
+    // return the popped data
+    return record;
+  }
+
+  // create a peek method
+  peek() {
+    // while the first stack has data
+    while (this.first.peek()) {
+      // push the first stack data into the second stack
+      this.second.push(this.first.pop());
+    }
+
+    // peek the second stack data
+    const record = this.second.peek();
+
+    // while the second stack has data
+    while (this.second.peek()) {
+      // push the second stack data into the first stack
+      this.first.push(this.second.pop());
+    }
+
+    // return the peeked data
+    return record;
+  }
+}
 
 module.exports = Queue;
