38c70eed30ace5cda1bd5eda1ade07c026d202c1
dcollis92 <davidphilipcollis@gmail.com>
2023-01-16 16:26:43 -0600

    define node class

--- udemy/coding-interview-bootcamp/AlgoCasts/exercises/21-linkedlist/index.js
+++ udemy/coding-interview-bootcamp/AlgoCasts/exercises/21-linkedlist/index.js
@@ -2,7 +2,14 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+  constructor(data, next = null) {
+    // create a data property assigning it to the data argument
+    this.data = data;
+    // create a next property assigning it to the next argument
+    this.next = next;
+  }
+}
 
 class LinkedList {}
 
